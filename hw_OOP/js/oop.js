8d3e8a1a52f136006766877cefe4ad759580f07c
Dmitriy <rusnak179@gmail.com>
2016-11-29 22:32:22 +0200

    hw_OOP some remarks

--- hw_OOP/js/oop.js
+++ hw_OOP/js/oop.js
@@ -1,4 +1,262 @@
+ "use strict"
 function Figures(type){
+	this._setType=function(){
+		this._type = type;
+	}
+	
+	this._getType = function(){
+		return this._type;
+	}
+	this._setType();
+	
+	this._setRes=function(){
+		this._res = 0;
+	}
+	
+	this._getRes=function(){
+		return this._res;
+	}
+	this._setRes();
+	
+	this.square=function(){
+		if(this._getType()!=='undefined')
+			console.log("Square of " + this._getType() + " is " + this._getRes());
+		else
+			return;
+	}
+	
+	this.perimetr=function(){
+		if(this._getType()!=='undefined')
+			console.log("Perimetr of " + this._getType() + " is " + this._getRes());
+		else
+			return;
+	}
+}
+
+
+function Rectangle(type,a,b){
+	var res=0;
+	Figures.apply(this, arguments);
+	var setParentType = this._setType;
+	this._setType = function(){
+		setParentType.call(this);
+		if(type!='Rectangle'){
+			console.log('Type of the figure have to be "Rectangle"');
+			this._type = 'undefined';
+			return;
+		}
+		else{
+			this._type = type;
+		}
+	}
+	
+	this._setA=function(){
+		if(a && a>0)
+			this._a=a;
+		else
+			console.log('The length of the first side is wrong!');
+	};
+	
+	this._setB=function(){
+		if(b && b>0)
+			this._b=b;
+		else{
+			console.log('The length of the second side is wrong!');
+			res = 0;
+		}
+	};
+	
+	this._getA=function(){
+		return this._a;
+	};
+	
+	this._getB=function(){
+		return this._b;
+	};
+	
+	var parentFigureSq = this.square;
+	var perentSetRes = this._setRes;
+	
+	this.square = function(){
+		parentFigureSq.call(this);
+		this._setType();
+		if(this._getType()!=='undefined'){
+			this._setA();
+			this._setB();
+			this._setRes = function(){
+				perentSetRes.call(this);
+				if(this._a && this._b)
+					this._res = this._getA()*this._getB();
+				else
+					this._res = 0;
+			}
+			this._setRes();
+		}
+	}
+	var parentFigurePer = this.perimetr;
+	
+	this.perimetr = function(){
+		parentFigurePer.call(this);
+		this._setType();
+		if(this._getType()!=='undefined'){
+			this._setA();
+			this._setB();
+			this._setRes = function(){
+				perentSetRes.call(this);
+				if(this._a && this._b)
+					this._res = 2*this._getA()+2*this._getB();
+				else
+					this._res = 0;
+			}
+			this._setRes();
+		}
+	}
+}
+
+
+var fig2 = new Rectangle('Rectangle',10,15);
+console.log(fig2)
+fig2.square();
+/*fig2.perimetr();*/
+
+
+/*function Circle(type,rad){
+	var pi = 3.1415;
+	var res=0;
+	Figures.apply(this, arguments);
+	this._setType = function(){
+		if(type!='Circle'){
+			console.log('Type of the figure have to be "Circle"');
+			this._type = 'undefined';
+			return;
+		}
+		else{
+			this._type = type;
+		}
+	}
+	this._setRad = function(){
+		if(rad<0){
+			console.log("Radius have to be more then '0'");
+		}
+		else{
+			this._rad = rad;
+		}
+	}
+	this._getRad = function(){
+		return this._rad;
+	}
+	var parentFigureSq = this.square;
+	this.square = function(){
+		parentFigureSq.call(this);
+		this._setType();
+		if(this._getType()!='undefined'){
+			this._setRad();
+			res = pi*this._getRad()*this._getRad();
+			console.log(res);
+		}
+	}
+	var parentFigurePer = this.perimetr;
+	this.perimetr = function(){
+		parentFigurePer.call(this);
+		this._setType();
+		if(this._getType()!='undefined'){
+			this._setRad();
+			res = 2*pi*this._getRad();
+			console.log(res);
+		}
+	}
+}
+
+
+function Square(type,a,b){
+	Rectangle.apply(this, arguments);
+}
+
+
+function Triangle(type,a,b,c){
+	var res=0;
+	Figures.apply(this, arguments);
+	
+	this._setType = function(){
+		if(type!='Triangle'){
+			console.log('Type of the figure have to be "Rectangle"');
+			this._type = 'undefined';
+			return;
+		}
+		else{
+			this._type = type;
+		}
+	};
+	
+	this._setA=function(){
+		if(a && a>0)
+			this._a=a;
+		else
+			console.log('The length of the first side is wrong!');
+	};
+	
+	this._setB=function(){
+		if(b && b>0)
+			this._b=b;
+		else
+			console.log('The length of the second side is wrong!');
+	};
+	
+	this._setC=function(){
+		if(c && c>0)
+			this._c=c;
+		else
+			console.log('The length of the third side is wrong!');
+	};
+	
+	this._getA=function(){
+		return this._a;
+	};
+	
+	this._getB=function(){
+		return this._b;
+	};
+	
+	this._getC=function(){
+		return this._c;
+	};
+	
+	var parentFigureSq = this.square;
+	
+	this.square = function(){
+		parentFigureSq.call(this);
+		this._setType();
+		if(this._getType()!='undefined'){
+			this._setA();
+			this._setB();
+			this._setC();
+			var halfPerim = (this._getA() + this._getB() + this._getC())/2;
+			res = Math.sqrt(halfPerim*(halfPerim-this._getA())*(halfPerim-this._getB())*(halfPerim-this._getC()));
+			console.log(res);
+		}
+	};
+	
+	var parentFigurePer = this.perimetr;
+	this.perimetr = function(){
+		parentFigurePer.call(this);
+		this._setType();
+		if(this._getType()!='undefined'){
+			this._setA();
+			this._setB();
+			this._setC();
+			res = this._getA() + this._getB() + this._getC();
+			console.log(res);
+		}
+	}
+}*/
+
+
+
+
+
+
+
+/*function Figures(type){
 	this._type=type;
 	this.square=function(){
 		console.log("Square of " + this._type + " is");
@@ -59,61 +317,5 @@ function Triangle(type,a,b,c){
 	this.perimetr = function(){
 		parentFigurePer.call(this);
 		console.log(this._a + this._b + this._c);
-	}
-}
-
-
-var fig1 = new Circle('circle',4);
-console.log(fig1)
-fig1.square();
-fig1.perimetr();
-var fig2 = new Rectangle('Rectangle',15,15);
-fig2.perimetr();
-var fig3 = new Triangle('Triangle',20,10,25);
-fig3.perimetr();
-fig3.square();
-var fig4 = new Square('Square',5,5);
-fig4.perimetr();
-fig4.square();
-
-/*function Figures(a,b,c,rad){
-	this._a=a;
-	this._b=b;
-	this._c=c;
-	this._rad=rad;
-	this._pi = 3.1415;
-	this.square=function(){
-		if(this._rad)
-			console.log(this._pi*this._rad*this._rad);
-		else if(this._a && this._b && !this._c)
-			console.log(this._a*this._b);
-		else
-			console.log(this._a*this._a); 
-	}
-	this.perimetr=function(){
-		if(this._rad)
-			console.log(2*this._pi*this._rad);
-		else if(this._a && this._b && !this._c)
-			console.log(2*this._a + 2*this._b);
-		else if(this._c)
-			console.log(this._a + this._b + this._c);
-		else
-			console.log(4*this._a);
-	}
-}
-
-function Circle(rad){
-	Figures.apply(this, arguments);
-}
-
-function Rectangle(a,b){
-	Figures.apply(this, arguments);
-}
-
-function Triangle(a,b,c){
-	Figures.apply(this, arguments);
-	var halfPerim = (this._a + this._b + this._c)/2;
-	this.square=function(){
-		return Math.sqrt(halfPerim*(halfPerim-this._a)*(halfPerim-this._b)*(halfPerim-this._c)); 
 	}
 }*/
